6afbc6fd87338ceb6e06ea6b734ce79a8eca79fb
vgmoose <vgmoose@gmail.com>
2016-06-25 16:51:15 -0400

    changes by @pwsincd: web frontend, sorting+filtering

--- web/gen.py
+++ web/gen.py
@@ -20,12 +20,13 @@ print "Content-type: text/html\n\n"
 print "<html>Updating Appstore......<br><br></html>"
 
 style = """<head>
+<!-- javascript and css styling for the icon tooltip and js for sorting-->
     <link rel="stylesheet" type="text/css" href="css/tooltipster.bundle.min.css" />
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.0.min.js"></script>
     <script type="text/javascript" src="js/tooltipster.bundle.min.js"></script>
-    <script type="text/javascript" src="js/tsorter.min.js"></script>
+    <script type="text/javascript" src="js/sorttable.js"></script>
 <style>
-
+/*table style*/
 #wiiubru {
     font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
     border-collapse: collapse;
@@ -41,13 +42,12 @@ style = """<head>
 { 
    -webkit-transition: -webkit-transform .2s ease-in-out;
     -webkit-transform: scale(0.9, 0.77);
-    z-index: 999;
 }
 
 #wiiubru tr:nth-child(even){background-color: #2a2a2a}
 #wiiubru tr:nth-child(odd){background-color: #3f3f3f}
-a:link {color: red;}
-a:visited {color: red;}
+a:link {color: #b30000;}
+a:visited {color: #b30000;}
 a:hover {color: white;}
 
 #wiiubru tr:hover {
@@ -58,8 +58,10 @@ a:hover {color: white;}
 #wiiubru th {
     padding-top: 12px;
     padding-bottom: 12px;
-    background-color: #7e7e7e;
+    background-color: #b30000;
+
 }
+/*icon stretch*/
 .image 
 {
    -webkit-transition: -webkit-transform .2s ease-in-out;
@@ -68,16 +70,56 @@ a:hover {color: white;}
 }
 .image:hover
 {
-   z-index: 1;
    -webkit-transition: -webkit-transform .2s ease-in-out;
     -webkit-transform: scale(1.5, 1.5);
 }
 body
 {
     background-image: url("bg.png");
+/*table sort icons .. to be continued*/
+}
+table.sortable th::after, th.sorttable_sorted::after, th.sorttable_sorted_reverse::after {
+  content: " ";
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+}
+th.sorttable_sorted::after {
+  background: url(my-sorted-icon.png);
+  background-size: contain;
+}
+th.sorttable_sorted_reverse::after {
+  background: url(my-sorted-reversed-icon.png);
+  background-size: cover;
+}
+/*search box styling*/
+input[type=text]
+{
+    width: 130px;
+    box-sizing: border-box;
+    border: 2px solid #b30000;
+    border-radius: 4px;
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+    background-color: #3f3f3f;
+    background-image: url("search.png");
+    background-position: 5px 8px;
+    background-repeat: no-repeat;
+    padding: 10px 30px 10px 40px;
+    -webkit-transition: width 0.4s ease-in-out;
+    transition: width 0.4s ease-in-out;
 }
 
+input[type=text]:focus {
+    width: 1000;
+    background-image: url("searchex.png");
+    placeholder: 'Search for an app.....';
+}
+
+
 </style>
+<!-- tooltip script to display the homebrews long_description when mousing over the table cell -->
     <script>
         $(document).ready(function() {
             $('.tooltip').tooltipster({
@@ -91,15 +133,34 @@ body
 
     </script>
 </head>
+<!-- website header logo and search box -->
 <body>
 <div style="text-align: center; width=100%;">
 <a href="#">
 <img src="hbasheader.png" height=161 width=1000>
 </a>
+<br>
+</div>
+<div style='font-family: "Trebuchet MS", Arial, Helvetica, sans-serif; text-align: center; width=100%; color: white;'>
+<input type="text" id="search" placeholder=""><br>
 </div>
 """
+search = """
+<!-- search script -->
+<script>
+var $rows = $('#wiiubru tbody tr');
+$('#search').keyup(function() {
+    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+    
+    $rows.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(val);
+    }).hide();
+});
+</script>
+"""
 
-html = style + "<table id='wiiubru'><tr><th>ICON</th><th>TITLE</th><th>AUTHOR</th><th>DESCRIPTION</th><th>DOWNLOAD</th><th>SOURCE</th></tr>\n"
+html = style + "<table id='wiiubru' class='sortable'><thead><tr><th class='sorttable_nosort'>ICON</th><th style = 'cursor:pointer;'>TITLE</th><th class='sorttable_nosort'>VERSION</th><th style = 'cursor:pointer;'>AUTHOR</th><th style = 'cursor:pointer;'>DESCRIPTION</th><th class='sorttable_nosort'>DOWNLOAD</th><th class='sorttable_nosort'>SOURCE</th></tr></thead><tbody>\n"
 yaml = ""
     
 try:
@@ -185,7 +246,7 @@ for app in os.listdir("apps"):
 
     d[app] = {"name": name, "author": coder, "desc": desc, "url": src_link, "binary": binary, "long_desc": long_desc}
 
-    html += "<tr><td class='tooltip' title='%s'><img src='%s' class='image'></td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n" % (long_desc, icon, name, coder, desc, dlhref, src_link)
+    html += "<tr><td class='tooltip' title='%s'><img src='%s' class='image'></td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n" % (long_desc, icon, name, version, coder, desc, dlhref, src_link)
     
     yaml += "app: %s\n- %s\n- %s\n- %s\n- %s\n- %s\n" % (app, name, coder, desc, binary, version)
     
@@ -195,7 +256,8 @@ for app in os.listdir("apps"):
         zipf.close()
 
 
-html += "</table></body>"
+html += "</tbody></table></body>"
+html += search
 
 jsonstring = json.dumps(d, indent=4, separators=(',', ': '))
 
