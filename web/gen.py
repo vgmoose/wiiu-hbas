a212711eed13959ad7257fd26244bfc50065d742
pwsincd <pwsincd@gmail.com>
2016-08-06 18:47:13 +0100

    Current Wiiubru config
    Several design edits

--- web/gen.py
+++ web/gen.py
@@ -32,6 +32,7 @@ style = """<!DOCTYPE html>
     <script type="text/javascript" src="js/tooltipster.bundle.min.js"></script>
     <script type="text/javascript" src="js/sorttable.js"></script>
 
+
 <!-- tooltip script to display the homebrews long_description when mousing over the icon table cell -->
     <script>
         $(document).ready(function() {
@@ -45,20 +46,31 @@ style = """<!DOCTYPE html>
         });
     </script>
 
+
 </head>
 <body>
 <header>
-<span class='pull-left'><input type='text' id='search' placeholder='&#xF002;' style='font-family:Arial, FontAwesome'></span>
-<span class='pull-right'>
-<a href="https://github.com/vgmoose/hbas" target='_blank'>
-<i class='fa fa-github fa-2x' aria-hidden="true"></i>
-</a>
+<ul>
+<li style='float: left'><img src='http://www.wiiubru.com/forum/forum/styles/digi_red/theme/images/logo.png' alt='wiiubru' width='95px' height='45px'></img></li>
+<li style='float: left'><input type='text' id='search' placeholder='&#xF002;' style='font-family:Arial, FontAwesome'></li>
 
-<a href="https://kiwiirc.com/client?settings=76b79dc4baeccc495e0b78d2a052f984" target='_blank'>
-<i class="fa fa-comments-o fa-2x" aria-hidden="true"></i>
-</a>
+  <li class="dropdown">
+    <a href="#" class="dropbtn"><i class="fa fa-link" aria-hidden="true"></i> USEFUL LINKS</a>
+    <div class="dropdown-content">
+      <a href="https://gbatemp.net/threads/release-homebrew-app-store-for-wiiu.433275/" target='_blank'><i class="fa fa-external-link-square" aria-hidden="true"></i> HBAS GBAtemp</a>
+      <a href="https://gbatemp.net/threads/www-wiiubru-com-homebrew-launcher-website-and-5-5-exploit-hosting.411691/" target='_blank'><i class="fa fa-external-link-square" aria-hidden="true"></i> WiiUbru GBAtemp</a>
+      <a href="#" target='_blank'><i class="fa fa-external-link-square" aria-hidden="true"></i> Reserved</a>
+    </div>
+  </li>
+   <li><a href="https://www.paypal.me/pwsincd" target='_blank'><i class="fa fa-paypal" aria-hidden="true"></i> DONATE</a></li>
+  <li><a href="http://www.wiiubru.com/appstore/starterkit/starter_pack.rar"><i class="fa fa-download" aria-hidden="true"></i> STARTER PACK</a></li>
+  <li><a href="https://github.com/vgmoose/hbas" target='_blank'><i class="fa fa-github" aria-hidden="true"></i> GIT</a></li>
+  <li><a href="https://kiwiirc.com/client/irc.kiwiirc.com/wiiubru" target='_blank'><i class="fa fa-comments-o" aria-hidden="true"></i> IRC</a></li>
+  <li><a href="http://forum.wiiubru.com/" target='_blank'><i class="fa fa-forumbee" aria-hidden="true"></i> FORUM</a></li>
+  <li><a href="http://www.wiiubru.com/ask" target='_blank'><i class="fa fa-question" aria-hidden="true"></i> ASK</a></li>
+  <li><a href="http://www.wiiubru.com/appstore/stats/index.html" target='_blank'><i class="fa fa-line-chart" aria-hidden="true"></i> STATS</a></li>
 
-</span>
+</ul>
 </header>
 
 <br>
@@ -68,11 +80,23 @@ style = """<!DOCTYPE html>
 </a>
 <br>
 </div>
+<div align="center">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- wiiubru -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-7623664678937879"
+     data-ad-slot="9058831412"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
 
 """
 search = """
-<!-- search script -->
+
 <script>
+// search script
 var $rows = $('#wiiubru tbody tr');
 $('#search').keyup(function() {
     var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
@@ -82,6 +106,7 @@ $('#search').keyup(function() {
         return !~text.indexOf(val);
     }).hide();
 });
+
 </script>
 """
 
@@ -92,19 +117,39 @@ footer = """
 You are currently viewing the web front-end to the Homebrew App Store.
 </span>
 <span class='pull-left'>
-<a href="https://github.com/vgmoose/hbas" target='_blank'>
-<i class='fa fa-github fa-2x' aria-hidden='true'></i>
+<a href="http://www.wiiubru.com/appstore/stats/index.html" target='_blank' title='STATS'>
+<i class="fa fa-line-chart fa-2x" aria-hidden="true"></i>
 </a>
-
-<a href="https://kiwiirc.com/client?settings=76b79dc4baeccc495e0b78d2a052f984" target='_blank'>
+.
+<a href="http://forum.wiiubru.com/" target='_blank' title='FORUMS'>
+<i class="fa fa-forumbee fa-2x" aria-hidden="true"></i>
+</a>
+.
+<a href="https://kiwiirc.com/client?settings=76b79dc4baeccc495e0b78d2a052f984" target='_blank' title='IRC'>
 <i class="fa fa-comments-o fa-2x" aria-hidden="true"></i>
 </a>
+.
+<a href="https://github.com/vgmoose/hbas" target='_blank' title='GITHUB'>
+<i class='fa fa-github fa-2x' aria-hidden='true'></i>
+</a>
+.
+<a href="http://www.wiiubru.com/appstore/starterkit/starter_pack.rar" target='_blank' title='STARTER PACK'>
+<i class="fa fa-download fa-2x" aria-hidden="true"></i>
+</a>
+.
+<a href="https://www.paypal.me/pwsincd" target='_blank' title='DONATE'>
+<i class="fa fa-paypal fa-2x" aria-hidden="true"></i>
+</a>
+<a href="https://www.wiiubru.com/ask" target='_blank' title='ASK'>
+<i class="fa fa-question fa-2x" aria-hidden="true"></i>
+</a>
+
 
 </span>
 </footer>
 """
 
-html = style + "<table id='wiiubru' class='sortable'><thead><tr><th class='sorttable_nosort'>ICON</th><th style = 'cursor:pointer;' title = 'Click to sort.'>TITLE</th><th class='sorttable_nosort'>VERSION</th><th style = 'cursor:pointer;' title = 'Click to sort.'>AUTHOR</th><th style = 'cursor:pointer;' title = 'Click to sort.'>DESCRIPTION</th><th class='sorttable_nosort'>DOWNLOAD</th><th class='sorttable_nosort'>SOURCE</th></tr></thead><tbody>\n"
+html = style + "<table id='wiiubru' class='sortable'><thead><tr><th class='sorttable_nosort'>ICON</th><th style = 'cursor:pointer;' title = 'Click to sort.'><i class='fa fa-sort' aria-hidden='true'></i> TITLE</th><th class='sorttable_nosort'>VERSION</th><th style = 'cursor:pointer;' title = 'Click to sort.'><i class='fa fa-sort' aria-hidden='true'></i> AUTHOR</th><th style = 'cursor:pointer;' title = 'Click to sort.'><i class='fa fa-sort' aria-hidden='true'></i> DESCRIPTION</th><th class='sorttable_nosort'>DOWNLOAD</th><th class='sorttable_nosort'>SOURCE</th></tr></thead><tbody>\n"
 yaml = ""
     
 try:
@@ -206,8 +251,8 @@ for app in os.listdir("apps"):
 html += "</tbody></table><br><br>"
 html += search
 html += footer
-html += "</body>"
-html += "</html>"
+html += "</body></html>"
+
 
 
 jsonstring = json.dumps(d, indent=4, separators=(',', ': '))
@@ -227,7 +272,7 @@ jsonout.close()
 print"<html><br> Update Complete !!!<br><br></html>"
 print "<a href='index.html'>See the Results.</a>"
 
-#xml3 = open("directory.xml", "w+")
-#xml3.write(dict2xml(d))
-#xml3.close()
+xml3 = open("directory.xml", "w+")
+xml3.write(dict2xml(d))
+xml3.close()
 
