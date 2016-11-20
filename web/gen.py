ce1d4adc229b3ec8213763c8e03471111a8d63a6
vgmoose <me@vgmoose.com>
2016-11-20 09:13:20 -0500

    add formatted error output to gen.py

--- web/gen.py
+++ web/gen.py
@@ -7,160 +7,171 @@
 # in particiular, this script will generate homebrew zips,
 # icon zips, and a directory json
 
-import os, json, zipfile, datetime, time, imghdr
+import os, json, zipfile, datetime, time, imghdr, traceback, sys, ast
+from pprint import pformat
 import xml.etree.ElementTree as ET
 
 print "Content-type: text/html\n\n"
 
-# list of featured apps, these will be moved to the top of the json file
-featured = ["asturoids", "spacegame", "flappy_bird", "homebrew_launcher", "loadiine_gx2", "retro_launcher", "pong", "pacman", "snake", "mgba", "CHIP8", "PokeMiniU", "saviine", "ftpiiu", "cfwbooter", "appstore", "geckiine", "u-paint", "hid_keyboard_monitor", "LiveSynthesisU", "keyboard_example", "Snes9x2010"]
-
-# This function zips the incoming path into the file in ziph
-def zipdir(path, ziph):
-	for root, dirs, files in os.walk(path):
-		for file in files:
-			ziph.write(os.path.join(root, file))
-			
-# This function parses out several attributes from xml
-def xml_read(incoming):
-	outgoing = []
-	for key in incoming:
-		try:
-			outgoing.append(tree.find(key).text)
-		except:
-			outgoing.append("N/A")
-	return tuple(outgoing)
-
-# create the zips directory
+# try and catch ANY error so web has output
 try:
-	os.mkdir("zips")
-except:
-	pass
 
-# the resulting json output
-out = {}
-out["apps"] = []
+	# list of featured apps, these will be moved to the top of the json file
+	featured = ["asturoids", "spacegame", "flappy_bird", "homebrew_launcher", "loadiine_gx2", "retro_launcher", "pong", "pacman", "snake", "mgba", "CHIP8", "PokeMiniU", "saviine", "ftpiiu", "cfwbooter", "appstore", "geckiine", "u-paint", "hid_keyboard_monitor", "LiveSynthesisU", "keyboard_example", "Snes9x2010"]
 
-# read lasts updated info from a "cache.txt" file
-cache = {}
-try:
-	contents = open("cache.txt", "r")
-	for line in contents:
-		line = line.replace("\n", "").split("\t")
-		name = line[1]
-		updated = line[0]
-		cache[name] = updated
-except:
-	pass
+	# This function zips the incoming path into the file in ziph
+	def zipdir(path, ziph):
+		for root, dirs, files in os.walk(path):
+			for file in files:
+				ziph.write(os.path.join(root, file))
 
-apps = os.listdir("apps")
+	# This function parses out several attributes from xml
+	def xml_read(incoming):
+		outgoing = []
+		for key in incoming:
+			try:
+				outgoing.append(tree.find(key).text)
+			except:
+				outgoing.append("N/A")
+		return tuple(outgoing)
 
-for app in apps:
-	# ignore dotfiles
-	if app.startswith("."):
-		continue
-		
-	targdir = "apps"
-	
-	# get meta.xml file from HBL
-	xmlfile = targdir + "/%s/meta.xml" % app
-	-    
- -	# get icon.png file from HBL
- -	iconfile = targdir + "/%s/icon.png" % app
- -   
- -	if imghdr.what(iconfile) != "png":
- -		print "Skipping %s as its icon.png isn't a png file" % app
- -		continue
-
-	# create some default fields
-	name = coder = desc = long_desc = version = source = updated = filesize = category = src_link = "N/A"
-	typee = "hbl"
-
-	# find a binary in this app folder
-	binary = None
-	for file in os.listdir(targdir + "/%s" % app):
-		if file.endswith(".elf") or file.endswith(".rpx"):
-			binary = file
-			updated = time.ctime(os.path.getmtime(targdir + "/%s" % app))
-			# find filesize of binary
-			filesize = os.path.getsize(targdir + "/%s/%s" % (app,binary))
-
-
-
-
-	# if there's no binary found, continue
-	if not binary:
-		continue
-
-	# make sure rpxes are categorized as such
-	if binary.endswith(".rpx"):
-		typee = "rpx"
-
-	# get fields out of xml
-	if os.path.isfile(xmlfile):
-		# parse the xml file
-		tree = ET.parse(xmlfile)
-
-		# pull out those attributes
-		name, coder, desc, source, long_desc, version, category = xml_read(["name", "coder", "short_description", "url", "long_description", "version", "category"])
-		
-	# sanitize long_desc for json output
-	long_desc = long_desc.replace("\n", "\\n").replace("\t", "\\t")
-	
-	# get icon path
-	icon = targdir + "/%s/icon.png" % app
-
-	# append to output json
-	out["apps"].append({"filesize": filesize, "updated": updated, "directory": app, "name": name, "author": coder, "desc": desc, "url": source, "binary": binary, "long_desc": long_desc, "type": typee, "cat": category})
-	
-			
-	# if there's no update according to the cache
-	if app in cache and cache[app] == updated:
-		continue
-			
-	print "Compressing " + app + "...<br>"
-
-	# zip up this app
-	zipf = zipfile.ZipFile("zips/%s.zip" % app, 'w', zipfile.ZIP_DEFLATED)
-	zipdir(targdir + "/%s" % app, zipf)
-	zipf.close()
-
-# last updated date
-out["updated"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-# move "featured" apps to the front if available
-apps = out["apps"]
-for app in featured[::-1]:
+	# create the zips directory
 	try:
-		for rapp in apps:
-			if rapp["directory"] == app:
-				apps.insert(0, apps.pop(apps.index(rapp)))
-				continue
+		os.mkdir("zips")
 	except:
 		pass
 
-# json string formatting
-jsonstring = json.dumps(out, indent=4, separators=(',', ': '))
+	# the resulting json output
+	out = {}
+	out["apps"] = []
 
-# write it to file
-jsonout = open("directory.json", "w+")
-jsonout.write(jsonstring)
-jsonout.close()
+	# read lasts updated info from a "cache.txt" file
+	cache = {}
+	try:
+		contents = open("cache.txt", "r")
+		for line in contents:
+			line = line.replace("\n", "").split("\t")
+			name = line[1]
+			updated = line[0]
+			cache[name] = updated
+	except:
+		pass
 
-# compress the directory.json and app icons
-# to make downloading faster
-ziph = zipfile.ZipFile("directory.zip", 'w', zipfile.ZIP_DEFLATED)
-ziph.write("directory.json")
-for cur in out["apps"]:
-	ziph.write("apps/"+cur["directory"]+"/icon.png", cur["directory"]+".png")
-ziph.close()
+	apps = os.listdir("apps")
 
-# write out the update times to a cache file
-cache_out = open("cache.txt", "w")
-for cur in out["apps"]:	cache_out.write(cur["updated"]+"\t"+cur["directory"]+"\n")
-cache_out.close()
+	for app in apps:
+		# ignore dotfiles
+		if app.startswith("."):
+			continue
 
-# print done and run legacy scripts
-print "Updated directory.json !!!<br>"
-print "Running v1_gen.py for hbas 1.0...<br>"
-os.system("python v1_gen.py")
+		targdir = "apps"
+
+		# get meta.xml file from HBL
+		xmlfile = targdir + "/%s/meta.xml" % app
+
+		# get icon.png file from HBL
+		iconfile = targdir + "/%s/icon.png" % app
+
+		if imghdr.what(iconfile) != "png":
+			print "Skipping %s as its icon.png isn't a png file" % app
+			continue
+
+		# create some default fields
+		name = coder = desc = long_desc = version = source = updated = filesize = category = src_link = "N/A"
+		typee = "hbl"
+
+		# find a binary in this app folder
+		binary = None
+		for file in os.listdir(targdir + "/%s" % app):
+			if file.endswith(".elf") or file.endswith(".rpx"):
+				binary = file
+				updated = time.ctime(os.path.getmtime(targdir + "/%s" % app))
+				# find filesize of binary
+				filesize = os.path.getsize(targdir + "/%s/%s" % (app,binary))
+
+		# if there's no binary found, continue
+		if not binary:
+			continue
+
+		# make sure rpxes are categorized as such
+		if binary.endswith(".rpx"):
+			typee = "rpx"
+
+		# get fields out of xml
+		if os.path.isfile(xmlfile):
+			# parse the xml file
+			tree = ET.parse(xmlfile)
+
+			# pull out those attributes
+			name, coder, desc, source, long_desc, version, category = xml_read(["name", "coder", "short_description", "url", "long_description", "version", "category"])
+
+		# sanitize long_desc for json output
+		long_desc = long_desc.replace("\n", "\\n").replace("\t", "\\t")
+
+		# get icon path
+		icon = targdir + "/%s/icon.png" % app
+
+		# append to output json
+		out["apps"].append({"filesize": filesize, "updated": updated, "directory": app, "name": name, "author": coder, "desc": desc, "url": source, "binary": binary, "long_desc": long_desc, "type": typee, "cat": category})
+
+
+		# if there's no update according to the cache
+		if app in cache and cache[app] == updated:
+			continue
+
+		print "Compressing " + app + "...<br>"
+
+		# zip up this app
+		zipf = zipfile.ZipFile("zips/%s.zip" % app, 'w', zipfile.ZIP_DEFLATED)
+		zipdir(targdir + "/%s" % app, zipf)
+		zipf.close()
+
+	# last updated date
+	out["updated"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+	# move "featured" apps to the front if available
+	apps = out["apps"]
+	for app in featured[::-1]:
+		try:
+			for rapp in apps:
+				if rapp["directory"] == app:
+					apps.insert(0, apps.pop(apps.index(rapp)))
+					continue
+		except:
+			pass
+
+	# json string formatting
+	jsonstring = json.dumps(out, indent=4, separators=(',', ': '))
+
+	# write it to file
+	jsonout = open("directory.json", "w+")
+	jsonout.write(jsonstring)
+	jsonout.close()
+
+	# compress the directory.json and app icons
+	# to make downloading faster
+	ziph = zipfile.ZipFile("directory.zip", 'w', zipfile.ZIP_DEFLATED)
+	ziph.write("directory.json")
+	for cur in out["apps"]:
+		ziph.write("apps/"+cur["directory"]+"/icon.png", cur["directory"]+".png")
+	ziph.close()
+
+	# write out the update times to a cache file
+	cache_out = open("cache.txt", "w")
+	for cur in out["apps"]:	cache_out.write(cur["updated"]+"\t"+cur["directory"]+"\n")
+	cache_out.close()
+
+	# print done and run legacy scripts
+	print "Updated directory.json !!!<br>"
+	
+	print "Running v1_gen.py for hbas 1.0...<br>"
+	os.system("python v1_gen.py")
+
+except Exception, err:
+	exc_type, exc_value, exc_tb = sys.exc_info()
+	lines = pformat(traceback.format_exception(exc_type, exc_value, exc_tb))
+	
+	print "<font color='red'><h2>500 Error</h2>"
+	print "".join(ast.literal_eval(lines)).replace("\n", "<br>\n").replace("\\n", "<br>\n")
+	
+	
