4756c2ee4697b43c2e123de75ec64e5fcd5dd29a
vgmoose <me@vgmoose.com>
2017-05-24 16:20:40 -0400

    web: fix stripping of s, d, r, o, and t from start of .manifest files while zipping

--- web/gen.py
+++ web/gen.py
@@ -85,7 +85,7 @@ try:
 		for root, dirs, files in os.walk(path):
 			for file in files:
 				ppath = os.path.join(root, file)
-				ziph.write(ppath, ppath.lstrip("sdroot/"))
+				ziph.write(ppath, ppath.remove("sdroot/"))
 
 	# This function parses out several attributes from xml
 	def xml_read(incoming, tree, app):
